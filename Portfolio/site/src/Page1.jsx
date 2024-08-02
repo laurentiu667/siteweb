748bcb3e9d19493be5c64fb97d121cad28305b66
laurentiu667 <dilionlaur144@gmail.com>
2024-08-01 22:13:28 -0400

    change to wepb

--- Portfolio/site/src/Page1.jsx
+++ Portfolio/site/src/Page1.jsx
@@ -63,7 +63,7 @@ function Page1() {
                                 <div className="step-info">
                                     <h4>Step 3</h4>
                                     <p>
-                                        coool1cbon
+                                        coool1cbonok
                                     </p>
                                 </div>
                             </div>
