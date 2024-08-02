46f91f7a6bae152a62834099f1034aaf89ddbca3
laurentiu667 <dilionlaur144@gmail.com>
2024-08-01 22:14:18 -0400

    je crois c est bon je peux commencer a push quand je veux

--- Portfolio/site/src/Page1.jsx
+++ Portfolio/site/src/Page1.jsx
@@ -63,7 +63,7 @@ function Page1() {
                                 <div className="step-info">
                                     <h4>Step 3</h4>
                                     <p>
-                                        coool1cbonok
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing.
                                     </p>
                                 </div>
                             </div>
